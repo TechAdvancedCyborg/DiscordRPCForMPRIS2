51e8964ba3f222d895827e932d6f5abfe91e2c40
T.A.C.T.I.C.A.L <louis.dalibard@gmail.com>
2020-06-02 15:18:39 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from mpris2 import get_players_uri, Player
+import time
+from pypresence import Presence
+import time
+
+client_id = '717362726028050533'
+image = '81596775_p0'
+RPC = Presence(client_id)
+RPC.connect()
+
+uri = next(get_players_uri())
+player = Player(dbus_interface_info={'dbus_uri': uri})
+oldtrackid = "Null"
+while 1:
+     metadata = player.Metadata
+     if metadata['mpris:trackid'] != oldtrackid:
+         endtime = time.time()+metadata['mpris:length']/1000000
+         try:
+             RPC.update(state=metadata['xesam:artist'][0], details=metadata['xesam:title'],start=time.time(),large_image=image,end=endtime)
+         except:
+             RPC.update(details=metadata['xesam:title'],start=time.time(),large_image=image,end=endtime)
+         oldtrackid = metadata['mpris:trackid']
+     time.sleep(1)
