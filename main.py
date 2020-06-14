9d76c95491cd18a4defcdc79eb58a0d00db66f01
T.A.C.T.I.C.A.L <louis.dalibard@gmail.com>
2020-06-14 17:20:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from mpris2 import get_players_uri, Player
 import time
 from pypresence import Presence
-import time
+from termcolor import colored, cprint
 
 client_id = '717362726028050533'
 
@@ -12,30 +12,51 @@ RPC = Presence(client_id)
 RPC.connect()
 
 
-albumcovers = {"A Brief Inquiry Into Online Relationships":"abriefinquiry1","Good Faith":"good_faith","After Hours":"after_hours","Get Your Wish":"get_your_wish","Worlds":"worlds","":image}
-originicons = {"MellowPlayer":"mellowplayer","":"play-button"}
+albumcovers = {"A Brief Inquiry Into Online Relationships":"abriefinquiry1","Good Faith":"good_faith","After Hours":"after_hours","Get Your Wish":"get_your_wish","Worlds":"worlds","Illusions of the Heart":"illusions-of-the-heart","":image}
+originicons = {"MellowPlayer":"mellowplayer","":"play-button","pause-button":"pause-button"}
+
+def log(log_time,log_type,log_message):
+    log_message_to_print=str(round(time.time()*100)/100)
+    if log_type == "Log":
+        log_message_to_print = log_message_to_print+" [Log]: "+log_message
+    elif log_type == "Warning":
+        log_message_to_print = colored(log_message_to_print+" [Warning]: "+log_message, 'yellow')
+    elif log_type == "Error":
+        log_message_to_print = colored(log_message_to_print+" [Error]: "+log_message, 'red', attrs=['reverse'])
+    elif log_type == "Separator":
+        log_message_to_print="----------------------------------------\n"
+    elif log_type == "Validation":
+        log_message_to_print = colored(log_message,"green")
+    print(log_message_to_print, end = '')
+
+
+
+
 
 
 while 1:
     try:
-        print("----------------------------------------")
-        print(round(time.time()*100)/100,"Initialising DBUS Interface...")
+        log(time.time(),"Separator","")
+        log(time.time(),"Log","Initialising DBUS Interface")
         uri = next(get_players_uri())
         player = Player(dbus_interface_info={'dbus_uri': uri})
         oldtrackid = "Null"
         endtime=0
+        log(time.time(),"Validation","✓\n")
         while 1:
-             print("----------------------------------------")
-             print(round(time.time()*100)/100,"Getting from DBUS...")
+             log(time.time(),"Separator","")
+             log(time.time(),"Log","Getting from DBUS...")
+             log(time.time(),"Validation","✓\n")
              metadata = player.Metadata
              position = player.Position
              status = player.PlaybackStatus
-             print(round(time.time()*100)/100,"Processing...")
+             log(time.time(),"Log","Processing...")
+             log(time.time(),"Validation","✓\n")
              try:
                  if metadata['mpris:trackid'] != oldtrackid:
                      oldtrackid = metadata['mpris:trackid']
              except:
-                 pass
+                 log(time.time(),"Warning","No Track ID...\n")
              try:
                  currentalbumcover = ""
                  tempvar = albumcovers[str(metadata['xesam:album'])]
@@ -48,36 +69,45 @@ while 1:
                  currentoriginicon = str(metadata['origin'])
              except:
                  pass
-             try:
-                 if status == "Paused":
-                     currentoriginicon = "pause-button"
-             except:
-                 pass
+             if status == "Paused":
+                 currentoriginicon = "pause-button"
              try:
                  if str(metadata['xesam:artist'][0]) == "":
                      artist = "  "
+                     log(time.time(),"Warning","No Artist...\n")
                  else:
                      artist = metadata['xesam:artist'][0]
              except:
                  artist = "  "
+                 log(time.time(),"Warning","No Artist...\n")
              try:
                  album = " • "+metadata['xesam:album']
              except:
                  album = "  "
+                 log(time.time(),"Warning","No Album...\n")
              try:
                  title = metadata['xesam:title']
              except:
                  title = "  "
+                 log(time.time(),"Warning","No Title...\n")
              try:
                  origin = metadata['origin']
              except:
                  origin = "  "
-             dynamicendtime = time.time()+metadata['mpris:length']/1000000-position/1000000
-             print(round(time.time()*100)/100,"Updating RPC...")
+                 log(time.time(),"Warning","No Origin...\n")
+             try:
+                 dynamicendtime = time.time()+metadata['mpris:length']/1000000-position/1000000
+             except:
+                 log(time.time(),"Warning","No Length...\n")
+                 dynamicendtime = 100
+             log(time.time(),"Log","Updating RPC...")
+             log(time.time(),"Validation","✓\n")
              RPC.update(state=artist, details=title+album,start=time.time(),large_image=albumcovers[currentalbumcover],end=dynamicendtime,large_text=largetext,small_image=originicons[currentoriginicon],small_text=origin)
-             print(round(time.time()*100)/100,"Waiting 1s...")
+             log(time.time(),"Log","Waiting 1s...")
              time.sleep(1)
+             log(time.time(),"Validation","✓\n")
     except Exception as e:
-        print(round(time.time()*100)/100,"Error:",e)
-        print(round(time.time()*100)/100,"Waiting 5s...")
+        log(time.time(),"Error",str(e)+"\n")
+        log(time.time(),"Log","Waiting 5s...")
         time.sleep(5)
+        log(time.time(),"Validation","✓\n")
